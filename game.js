02636f98d1e0a35a3ba492986cbc2d33ce51fba6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-24 01:37:46 +0300

    adding the live timer to time the game in JS

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", playGame);
 window.addEventListener("load", createScoreBlock);
 
-let game_over = false; 
+let game_over = true; 
 let user_score = 0;
-
+let interval = 0;
 
 function playGame(){
-    game_over = false;
+
     let start = document.getElementById("start");
-    start.addEventListener("mouseenter", startGame);
+    start.addEventListener("mouseover", startGame);
 
     let restart = document.getElementById("start");
     restart.addEventListener("click", restartGame);
@@ -25,8 +25,9 @@ function playGame(){
     let out_of_bounds = document.getElementById("game");
     out_of_bounds.addEventListener("mouseleave", gotOutOfBounds);
 
-
     start.removeEventListener("mouseenter", playGame);
+    
+    
 
 }
     
@@ -39,6 +40,7 @@ function winGame(){
         game_status.style = "color: limegreen";
         user_score += 5;
         showScore();
+        
     }
 }
 
@@ -55,24 +57,27 @@ function loseGame(){
         game_status.style = "color: #ff8888";
         user_score -= 10;
         showScore();
-    }
+        clearInterval(interval);
 
+    }
 }
 
 function gotOutOfBounds(){
     if (!game_over){
         game_over = true;
         alert("You cheated!");
+        clearInterval(interval);
+
     }
 }
 
 function createScoreBlock(){
     score_box = document.createElement("h1");
     score_box.id = "score";
-    score_box.style.cssText = "text-align: center; font-size: 18px; font-weight: bold; color: dodgerblue;line-height: 1.em";
+    score_box.style.cssText = "text-align: center; font-size: 24px; font-weight: bold; color: dodgerblue;line-height: 1.em";
     objective = document.getElementsByTagName("p")[0];
     objective.parentNode.insertBefore(score_box, objective);
-    score_box.innerText = "Score:" + " " + user_score;
+    showScore();
 }
 
 
@@ -85,14 +90,20 @@ function showScore(){
 
 function startGame(){
     if (true){
+        let start_time = Date.now();
+    interval = setInterval(function(){
+        time_passed = Date.now() - start_time;
+        live_time = document.getElementById("live");
+        live_time.innerText = "Live" + " " + (time_passed/1000).toFixed(2);
+        },100);
+        let game_status = document.getElementById("status");
+        game_status.innerText = "Game On!";
+        game_over = false;
         boundaries = document.getElementsByClassName("boundary");
         let n = boundaries.length;
         for (let i = 0; i < n -1; i++){
             boundaries[i].classList.remove("youlose");
         }
-        let game_status = document.getElementById("status");
-        game_status.innerText = "Begin by moving your mouse over the \"S\".";
-        game_status.style = "color: black";
         playGame();
     }
 }
@@ -102,7 +113,6 @@ function startGame(){
 function restartGame(){
     user_score = 0;
     showScore();
-    restartGame();
-    game_over = true;
 
 }
+
